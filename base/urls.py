bfbce7ca6ff91f3f759c6f3fd64adf93bed171ba
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-19 18:17:09 +0800

    create crud operation and login form

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='task'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete')
+
 ]
