d6019ce7ec2657a27a0764f7234b7269383d2390
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-20 19:29:33 +0800

    debugging

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -134,8 +134,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-SESSION_ENGINE = "django.contrib.sessions.backends.db"
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_HTTPONLY = True
